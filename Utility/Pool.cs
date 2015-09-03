fab5550373036eeface9dd399aa2a7c923c57749
xgundam05 <anderson_matthew@live.com>
2015-09-03 18:25:03 -0400

    Fix Whitespace.. -.-

--- Utility/Pool.cs
+++ Utility/Pool.cs
@@ -4,42 +4,42 @@ using System.Collections.Generic;
 
 /// <summary>Dead Simple Object Pool</summary>
 public class Pool<T> where T : class, new() {
-	private Queue<T> _inactive;
-	private List<T> _all;
-	private Func<T> _generator;
+  private Queue<T> _inactive;
+  private List<T> _all;
+  private Func<T> _generator;
 
-	public Pool() : this(50)
-	{
-	}
+  public Pool() : this(50)
+  {
+  }
 
-	public Pool(int initialCap)
-		: this(initialCap, () => { return new T(); })
-	{
-	}
+  public Pool(int initialCap)
+    : this(initialCap, () => { return new T(); })
+  {
+  }
 
-	public Pool(int initialCap, Func<T> generator){
-		_inactive = new Queue<T>(initialCap);
-		_all = new List<T>(initialCap);
-		_generator = generator;
+  public Pool(int initialCap, Func<T> generator){
+    _inactive = new Queue<T>(initialCap);
+    _all = new List<T>(initialCap);
+    _generator = generator;
 
-		for (int i = 0; i < initialCap; i++){
-			T tmp = _generator();
-			_inactive.Enqueue(tmp);
-			_all.Add(tmp);
-		}
-	}
+    for (int i = 0; i < initialCap; i++){
+      T tmp = _generator();
+      _inactive.Enqueue(tmp);
+      _all.Add(tmp);
+    }
+  }
 
-	public T Request(){
-		if (_inactive.Count > 0)
-			return _inactive.Dequeue();
-		else {
-			T tmp = _generator();
-			_all.Add(tmp);
-			return tmp;
-		}
-	}
+  public T Request(){
+    if (_inactive.Count > 0)
+      return _inactive.Dequeue();
+    else {
+      T tmp = _generator();
+      _all.Add(tmp);
+      return tmp;
+    }
+  }
 
-	public void Return(T item){
-		_inactive.Enqueue(item);
-	}
+  public void Return(T item){
+    _inactive.Enqueue(item);
+  }
 }
--- Utility/PoolManager.cs
+++ Utility/PoolManager.cs
@@ -4,50 +4,50 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PoolManager : MonoBehaviour {
-	// Constants
-	private const int DEFAULT_CAPACITY = 100;
+  // Constants
+  private const int DEFAULT_CAPACITY = 100;
 
-	// Pool table -- Terrible Pun, but I couldn't resist >.>
-	private Dictionary<Type, object> _pools;
+  // Pool table -- Terrible Pun, but I couldn't resist >.>
+  private Dictionary<Type, object> _pools;
 
-	// Instance Member
-	private static PoolManager _current;
+  // Instance Member
+  private static PoolManager _current;
 
-	public static PoolManager current {
-		get {
-			if (_current == null){
-				_current = GameObject.FindObjectOfType<PoolManager>();
+  public static PoolManager current {
+    get {
+      if (_current == null){
+        _current = GameObject.FindObjectOfType<PoolManager>();
 
-				DontDestroyOnLoad(_current.gameObject);
-			}
+        DontDestroyOnLoad(_current.gameObject);
+      }
 
-			return _current;
-		}
-	}
+      return _current;
+    }
+  }
 
-	// Use this for initialization
-	void Awake () {
-		if (_current == null){
-			// We are the first! We must has the precious!
-			_current = this;
-			DontDestroyOnLoad(this);
-		}
-		else {
-			if (this != _current)
-				Destroy(this.gameObject);
-		}
-	}
+  // Use this for initialization
+  void Awake () {
+    if (_current == null){
+      // We are the first! We must has the precious!
+      _current = this;
+      DontDestroyOnLoad(this);
+    }
+    else {
+      if (this != _current)
+        Destroy(this.gameObject);
+    }
+  }
 
-	public Pool<T> Pools<T>() where T : class, new() {
-		if (this._pools == null)
-			this._pools = new Dictionary<Type, object>();
+  public Pool<T> Pools<T>() where T : class, new() {
+    if (this._pools == null)
+      this._pools = new Dictionary<Type, object>();
 
-		Type key = typeof(T);
-		if (!this._pools.ContainsKey(key)){
-			Pool<T> tmp = new Pool<T>(DEFAULT_CAPACITY);
-			this._pools.Add(key, tmp);
-		}
+    Type key = typeof(T);
+    if (!this._pools.ContainsKey(key)){
+      Pool<T> tmp = new Pool<T>(DEFAULT_CAPACITY);
+      this._pools.Add(key, tmp);
+    }
 
-		return this._pools[key] as Pool<T>;
-	}
+    return this._pools[key] as Pool<T>;
+  }
 }
